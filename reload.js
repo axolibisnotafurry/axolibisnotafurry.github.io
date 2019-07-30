d5d2b0667cd8a4b632c48b43af6ef8897ecd3f27
axolibisnotafurry <53496380+axolibisnotafurry@users.noreply.github.com>
2019-07-30 15:09:28 -0700

    Create reload.js

---
+++ reload.js
@@ -0,0 +1,11 @@
+var texts = [
+  "He really isn't.",
+  "Don't call him one.",
+  "Animal Crossing doesn't count.",
+  "Fireboltofdeath is a weeb.",
+  "I swear.",
+  "It's only for the memes.",
+  "He's also not a weeb.",
+];
+
+document.getElementById("mainText").innerHTML = texts[Math.floor(Math.random()*texts.length)];;
