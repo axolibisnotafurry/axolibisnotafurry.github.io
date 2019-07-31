c23babebeeffc72da79242996b8b4635a5bf258e
axolibisnotafurry <53496380+axolibisnotafurry@users.noreply.github.com>
2019-07-30 17:49:36 -0700

    Update reload.js

--- reload.js
+++ reload.js
@@ -11,3 +11,10 @@ var texts = [
 ];
 
 document.getElementById("mainText").innerHTML = texts[Math.floor(Math.random()*texts.length)];;
+
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
