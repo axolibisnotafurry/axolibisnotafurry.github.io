ead648b8580d327f46bc26f5f247109eb694dfe1
axolibisnotafurry <53496380+axolibisnotafurry@users.noreply.github.com>
2019-07-30 17:10:58 -0700

    Update reload.js

--- reload.js
+++ reload.js
@@ -7,6 +7,7 @@ var texts = [
   "It's only for the memes.",
   "He's also not a weeb.",
   "<img src='https://i.imgur.com/h7jh1g4.png'>",
+  '<iframe width="560" height="315" src="https://www.youtube.com/embed/Mz3Mi_OZYno?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
 ];
 
 document.getElementById("mainText").innerHTML = texts[Math.floor(Math.random()*texts.length)];;
